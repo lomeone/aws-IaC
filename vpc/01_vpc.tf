c7c862f02eb26762124303537d075cb224068c5a
comstering <coms1101@gmail.com>
2024-09-26 13:40:07 +0900

    feat: Set vpc resource

--- vpc/01_vpc.tf
+++ vpc/01_vpc.tf
@@ -1,15 +1,3 @@
-data "aws_availability_zones" "available" {
-  state = "available"
-}
-
-locals {
-  az = data.aws_availability_zones.available.names
-  subnet = {
-    "public"  = [for i in [10, 20] : cidrsubnet(var.cidr, 8, i)]
-    "private" = [for i in [30, 40] : cidrsubnet(var.cidr, 8, 1)]
-  }
-}
-
 resource "aws_vpc" "lomeone-vpc" {
   cidr_block = var.cidr
 
@@ -17,15 +5,6 @@ resource "aws_vpc" "lomeone-vpc" {
   enable_dns_support   = true
 
   tags = {
-    "kubernetes.io/role/internal-elb" = 1
-    Name                              = "${var.name}-vpc"
+    Name = "${var.vpc.name}-vpc"
   }
-}
-
-resource "aws_subnet" "public" {
-  vpc_id   = aws_vpc.lomeone-vpc.id
-  for_each = toset(local.subnet.public)
-
-  cidr_block        = each.value
-  availability_zone = local.az[index(local.subnet.public, each.value)]
 }
