6dc76adbd67bed9ce48475bae4425e33eb2acc06
comstering <coms1101@gmail.com>
2024-09-26 18:13:22 +0900

    feat: Add subnet resource file

---
+++ vpc/02_subnet.tf
@@ -0,0 +1,59 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  az = data.aws_availability_zones.available.names
+}
+
+resource "aws_subnet" "public" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.subnet_cidr.public)
+
+  cidr_block        = var.subnet_cidr.public[count.index]
+  availability_zone = local.az[count.index % var.availability_zone_count]
+
+  map_public_ip_on_launch = true
+
+  tags = {
+    "kubernetes.io/role/elb" = 1
+    Name                     = "${var.name.vpc}-${var.name.public_subnet}-${format("%02s", count.index)}-${local.az[count.index % var.availability_zone_count]}"
+  }
+}
+
+resource "aws_subnet" "eks_control_plane" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.subnet_cidr.eks_control_plane)
+
+  cidr_block        = var.subnet_cidr.eks_control_plane[count.index]
+  availability_zone = local.az[count.index % var.availability_zone_count]
+
+  tags = {
+    Name = "${var.name.vpc}-${var.name.eks_control_plane_subnet}-${format("%02s", count.index)}-${local.az[count.index % var.availability_zone_count]}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.subnet_cidr.private)
+
+  cidr_block        = var.subnet_cidr.private[count.index]
+  availability_zone = local.az[count.index % var.availability_zone_count]
+
+  tags = {
+    "kubernetes.io/role/interna-elb" = 1
+    Name                             = "${var.name.vpc}-${var.name.private_subnet}-${format("%02s", count.index)}-${local.az[count.index % var.availability_zone_count]}"
+  }
+}
+
+resource "aws_subnet" "db_private" {
+  vpc_id = aws_vpc.main.id
+  count  = length(var.subnet_cidr.db_private)
+
+  cidr_block        = var.subnet_cidr.db_private[count.index]
+  availability_zone = local.az[count.index % var.availability_zone_count]
+
+  tags = {
+    Name = "${var.name.vpc}-${var.name.db_private_subnet}-${format("%02s", count.index)}-${local.az[count.index % var.availability_zone_count]}"
+  }
+}
