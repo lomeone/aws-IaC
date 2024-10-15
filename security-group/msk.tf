7f25d90da5e087f5a23246586e592fb3c399af79
comstering <coms1101@gmail.com>
2024-10-15 21:00:52 +0900

    fix: Remove not use security group

--- security-group/msk.tf
+++ security-group/msk.tf
@@ -8,14 +8,6 @@ resource "aws_security_group" "msk_broker" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_msk_plaintext" {
-  security_group_id = aws_security_group.msk_broker.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 9092
-  to_port           = 9092
-  ip_protocol       = "tcp"
-}
-
 resource "aws_vpc_security_group_ingress_rule" "allow_msk_tls" {
   security_group_id = aws_security_group.msk_broker.id
   cidr_ipv4         = "0.0.0.0/0"
